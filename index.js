eb4997ec3667a809976b1911fcbb96c766a64a8f
Oleg Pudeyev <oleg@bsdpower.com>
2016-09-13 13:11:03 -0400

    Unnest the helper function

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // Based heavily on various answers given in
 // http://stackoverflow.com/questions/179355/clearing-all-cookies-with-javascript
+
+function clearAllPaths(location, cookieBase) {
+  var p = location.pathname.split('/');
+  document.cookie = cookieBase + '; path=/';
+  while (p.length > 0) {
+    document.cookie = cookieBase + '; path=' + p.join('/');
+    p.pop();
+  };
+}
+
 function clearAllCookies() {
-  var clearAllPaths = function(cookieBase) {
-    var p = location.pathname.split('/');
-    document.cookie = cookieBase + '; path=/';
-    while (p.length > 0) {
-      document.cookie = cookieBase + '; path=' + p.join('/');
-      p.pop();
-    };
-  }
-  
   var cookies = document.cookie.split("; ");
   for (var c = 0; c < cookies.length; c++) {
     var encodedCookieName = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]);
     var cookieBase = encodedCookieName + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT';
-    clearAllPaths(cookieBase);
+    clearAllPaths(window.location, cookieBase);
     
     var d = window.location.hostname.split(".");
     while (d.length > 0) {
-      clearAllPaths(cookieBase + '; domain=' + d.join('.'));
+      clearAllPaths(window.location, cookieBase + '; domain=' + d.join('.'));
       d.shift();
     }
   }
