bb52d9d6bbbf3c4f16711e7141289ff1a6df3fad
Oleg Pudeyev <oleg@bsdpower.com>
2016-09-13 13:11:23 -0400

    Take window as a parameter

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Based heavily on various answers given in
 // http://stackoverflow.com/questions/179355/clearing-all-cookies-with-javascript
 
-function clearAllPaths(location, cookieBase) {
-  var p = location.pathname.split('/');
-  document.cookie = cookieBase + '; path=/';
+function clearAllPaths(window, cookieBase) {
+  var p = window.location.pathname.split('/');
+  window.document.cookie = cookieBase + '; path=/';
   while (p.length > 0) {
-    document.cookie = cookieBase + '; path=' + p.join('/');
+    window.document.cookie = cookieBase + '; path=' + p.join('/');
     p.pop();
   };
 }
 
-function clearAllCookies() {
-  var cookies = document.cookie.split("; ");
+function clearAllCookies(window) {
+  var cookies = window.document.cookie.split("; ");
   for (var c = 0; c < cookies.length; c++) {
     var encodedCookieName = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]);
     var cookieBase = encodedCookieName + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT';
-    clearAllPaths(window.location, cookieBase);
+    clearAllPaths(window, cookieBase);
     
     var d = window.location.hostname.split(".");
     while (d.length > 0) {
-      clearAllPaths(window.location, cookieBase + '; domain=' + d.join('.'));
+      clearAllPaths(window, cookieBase + '; domain=' + d.join('.'));
       d.shift();
     }
   }
